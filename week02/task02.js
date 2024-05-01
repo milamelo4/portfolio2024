846df9da14dc1b5daa951a9cf0d7b36eb04e6972
milamelo4 <mel19009@byui.edu>
2024-05-01 13:20:49 -0600

    finished reading an task for week02

--- week02/task02.js
+++ week02/task02.js
@@ -192,16 +192,3 @@ createCalendar(document.getElementById("calendar"), 2012, 9);
 area.addEventListener('input', ()=> {
   localStorage.setItem("area", area.value);
 })
-
-// export an array
-export let months = ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-// export a constant
-export const MODULES_BECAME_STANDARD_YEAR = 2015;
-
-// export a class
-export class User {
-  constructor(name) {
-    this.name = name;
-  }
-}
